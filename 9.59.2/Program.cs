88ea4663f09e6896dfed6d264afa3a105e526e57
Андрей Гребенюк <andreygrebenyuk04@gmail.com>
2022-06-07 21:30:56 +0200

    Добавьте файлы проекта.

---
+++ 9.59.2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Гребенюк А., 21-ИСП-2, 9-я лабораторная, высокий уровень, 2-й вариант. 
+Console.WriteLine("Введите двоичное число: ");
+var str = Console.ReadLine();
+try
+{
+    long Two = Convert.ToInt32(str, 2);
+    string Eight = Convert.ToString(Two, 8);
+    Console.WriteLine(Eight);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
